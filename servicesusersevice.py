2a54b9924ef52fff659f3bca43c87c985430c760
이라온 <ilaon@ilaon-ui-MacBookPro.local>
2025-04-09 11:46:55 +0900

    Cleanup: remove duplicate user-related files from project root

--- servicesusersevice.py
+++
@@ -1,6 +0,0 @@
-from app.schemas.user import UserCreate, UserOut
-
-def create_user(user_data: UserCreate) -> UserOut:
-    # 예시용 가짜 유저 생성
-    return UserOut(id=1, email=user_data.email, username=user_data.username)
-
